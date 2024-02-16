93aabe36532bb9dc215faf388e317e4b3e28612a
AnxhelaAsimi <87004166+AnxhelaAsimi@users.noreply.github.com>
2024-02-16 15:38:32 +0100

    kj

--- frontend/src/slices/usersApiSlice.js
+++ frontend/src/slices/usersApiSlice.js
@@ -6,7 +6,7 @@ export const usersApiSlice = apiSlice.injectEndpoints({
     endpoints: (builder) => ({
         login: builder.mutation({
             query: (data) => ({
-             url: USERS_URL+'/auth',
+             url: `${USERS_URL}/auth`,
              method: 'POST',
              body: data
             }),
@@ -14,27 +14,27 @@ export const usersApiSlice = apiSlice.injectEndpoints({
         }),
         register: builder.mutation({
             query: (data) => ({
-             url: USERS_URL,
+             url: `${USERS_URL}`,
              method: 'POST',
              body: data
             }),
         }),
         logout: builder.mutation({
             query: () => ({
-             url: USERS_URL+'/logout',
+             url: `${USERS_URL}/logout`,
              method: 'POST',
             }),
         }),
         profile: builder.mutation({
             query: (data) => ({
-             url: USERS_URL+'/profile',
+             url: `${USERS_URL}/profile`,
              method: 'PUT',
              body: data
             }),
         }),
         getUsers: builder.query({
             query: () => ({
-              url: USERS_URL,
+              url: `${USERS_URL}`,
             }),
             providesTags: ['User'],
             keepUnusedDataFor: 5,
