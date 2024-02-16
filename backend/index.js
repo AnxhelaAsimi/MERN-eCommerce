2153a096fcda5d7f74561b2dc992d47af48ce1cc
AnxhelaAsimi <87004166+AnxhelaAsimi@users.noreply.github.com>
2024-02-16 16:11:56 +0100

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -43,12 +43,12 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-//   next();
-// });
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
+});
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
