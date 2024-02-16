e4ff7819462a8a770cc4a21123f4ef26b3b75351
AnxhelaAsimi <87004166+AnxhelaAsimi@users.noreply.github.com>
2024-02-16 15:16:42 +0100

    more desperate attempts

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,7 +1,7 @@
 // export const BASE_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:8000' : '';
-export const BASE_URL = 'https://mern-e-commerce-server-one.vercel.app/';
-export const PRODUCTS_URL = `https://mern-e-commerce-server-one.vercel.app/api/products/`;
-export const USERS_URL = `https://mern-e-commerce-server-one.vercel.app/api/users/`;
+export const BASE_URL = 'https://mern-e-commerce-server-one.vercel.app';
+export const PRODUCTS_URL = `https://mern-e-commerce-server-one.vercel.app/api/products`;
+export const USERS_URL = `https://mern-e-commerce-server-one.vercel.app/api/users`;
 export const ORDERS_URL = 'https://mern-e-commerce-server-one.vercel.app/api/orders';
 export const PAYPAL_CLIENT_ID = process.env.NODE_ENV === 'development' ? 'sb' : 'production';
 export const PAYPAL_URL = '/api/config/paypal';
